ec4d9f5711c2766b69f4bcb54ff01055426b8c40
Ryan Mooney <wm.ryan.mooney@gmail.com>
2021-08-23 19:54:50 -0800

    toggle test

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,32 @@
 <template>
-  <div class="col-md-6 col-lg-6 offset-md-3 ">
+  <div class="col-md-6 col-lg-6 offset-md-3" v-if="clientForm.displayClientForm">
     <ClientAdd />
   </div>
+  <button @click="toggleClientForm">Toggle</button>
 </template>
 
 <script>
 import ClientAdd from "@/components/forms/ClientAdd";
+import { reactive } from "vue";
 
 
 export default {
   name: 'Home',
   components: {
     ClientAdd
+  },
+  setup() {
+    const clientForm = reactive({
+      displayClientForm: true
+    })
+    const toggleClientForm = () => {
+      clientForm.displayClientForm = !clientForm.displayClientForm
+    }
+
+    return {
+      clientForm,
+      toggleClientForm
+    }
   }
 }
 </script>
